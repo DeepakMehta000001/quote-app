54c0b11fc9917321c40ce5df8377253efa4aee8b
arundeepak <arundeepak92@gmail.com>
2018-09-02 20:21:28 +0530

    added progressive bar and prod build

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
       <app-new-quote @quoteAdded="newQuote"></app-new-quote>
       <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
       <div class="row">
@@ -15,7 +16,7 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
-
+    import Header from './components/Header.vue';
     export default {
         data : function(){
           return {
@@ -27,6 +28,9 @@
         },
         methods : {
           newQuote(quote){
+            if(this.quotes.length >= this.maxQuotes){
+              return alert('Please delete quote first!');
+            }
             this.quotes.push(quote);
           },
           deleteQuote(index){
@@ -35,7 +39,8 @@
         },
         components: {
           appQuoteGrid: QuoteGrid,
-          appNewQuote : NewQuote
+          appNewQuote : NewQuote,
+          appHeader : Header
         }
     }
 </script>
