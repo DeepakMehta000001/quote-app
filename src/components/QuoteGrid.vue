32e7f3ab97cc6173b93cd565bba5ca8f5eeffa6b
arundeepak <arundeepak92@gmail.com>
2018-09-02 18:37:05 +0530

    added new quote support which shall have textarea and button and will emit new quote to the parent(QuoteGrid component)

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="row">
+    <app-new-quote></app-new-quote>
     <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
   </div>
 </template>
 
 <script>
   import Quote from './Quote.vue';
+  import NewQuote from './NewQuote.vue';
   export default{
     props: ['quotes'],
     components: {
-      appQuote: Quote
+      appQuote: Quote,
+      appNewQuote : NewQuote
     }
   }
 </script>
